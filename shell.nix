0de1487c134bacd6d857347dfc5e98cc892b880d
Giacomo Fraron <giacomo.fraron@avvale.com>
2026-04-27 15:42:28 +0200

    Edit per tracking on nix

--- shell.nix
+++ shell.nix
@@ -1,15 +1,19 @@
 { pkgs ? import <nixpkgs> {} }:
 let
-  java-package = pkgs.javaPackages.compiler.temurin-bin.jdk-25;
+  java25-package = pkgs.javaPackages.compiler.temurin-bin.jdk-25;
+  java25-bin = pkgs.writeShellScriptBin "java25" ''
+      exec ${java25-package}/bin/java "$@"
+    '';
 in
   pkgs.mkShellNoCC {
     packages = with pkgs; [
-      java-package
+      java25-package
+      java25-bin
       maven
     ];
 
     shellHook = ''
-      JAVA_HOME=${java-package}
+      JAVA_HOME=${java25-package}
       echo "❄️ NixShell is ready"
       echo "☕ Java $(java --version)"
       echo "📦 Maven $(mvn --version)"
